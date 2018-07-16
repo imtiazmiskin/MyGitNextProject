6319158efab0ce2826046128f713bebbba88f1a1
imtiaz <imtiazmiskin@gmail.com>
2018-07-16 22:04:08 +1000

    changes

--- TrnCountry.cs
+++ TrnCountry.cs
@@ -2,6 +2,7 @@ public class TrnCountry
 {
 	public static void main(string[] args)
 	{
+                Console.WriteLine("starting program");
 		Console.WriteLine("Hello World, How are you?");
 	}
 }
